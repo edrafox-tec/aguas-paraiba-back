fc10bbf79b527a5302f564d31f39126b64b47919
Tom <tom@edrafox.com>
2023-01-05 12:44:31 -0300

    build complete form

--- app/Http/Controllers/CompletedFormsController.php
+++ app/Http/Controllers/CompletedFormsController.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\answer;
-use App\Models\cr;
 use App\Models\form;
-use App\Models\formTheme;
-use App\Models\question;
-use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 
 class CompletedFormsController extends Controller
 {
     public function store($id, Request $request)
     {
-        $forms = form::findOrFail($id);
-        $formThemes = formTheme::where('id_form',$id)->get();
-        $questions = question::where('id_formTheme',$id)->get();
-        $answers = answer::where('id_question',$id)->get();
-        //return form::with('formThemes')->get();
-        $dados = form::with('formThemes')->get();
-        return $dados;
+        return form::findOrFail($id)->with('formThemes')->get();
     }
 }
