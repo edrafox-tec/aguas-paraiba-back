fe4766534920796ab019433915a6f442e5debde1
Tom <tom@edrafox.com>
2023-01-23 12:57:48 -0300

    fix list all reports

--- app/Http/Controllers/CompletedFormsController.php
+++ app/Http/Controllers/CompletedFormsController.php
@@ -54,7 +54,7 @@ class CompletedFormsController extends Controller
     }
     public function allAnswer()
     {
-       $ref = postWork::all();
+       $ref = postWork::with('form')->all();
        return $ref->toArray();//->where('id_user', $user)->get();
     }
 
