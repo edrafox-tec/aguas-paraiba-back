1b8487823a41090f4ae6c6006f12891657ba0b65
edrafox <85519212+edrafox-tec@users.noreply.github.com>
2023-03-01 08:53:21 -0300

    download pdf

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -28,7 +28,7 @@ class PdfController extends Controller
         if (count($array) > 0) {
             $title = $teste[0]['Form'];
             $pdf = PDF::loadView('pdf', compact('array', 'title', 'user'));
-            return $pdf->setPaper('a4')->stream($PostWork->created_at.'_'.$user->function .'_'.$title.'_'.$user->nickname . '.pdf');
+            return $pdf->setPaper('a4')->download($PostWork->created_at.'_'.$user->function .'_'.$title.'_'.$user->nickname . '.pdf');
         } else {
             return 'Formulário corrompido';
         }
